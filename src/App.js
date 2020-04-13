19eee76332685392e8263c2bd63ed370fa933f3a
igormpb <igor.mpb@hotmail.com>
2020-04-13 02:38:14 -0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Menu from './components/Menu/Menu'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-    
+      <Menu/>
     </div>
   );
 }
